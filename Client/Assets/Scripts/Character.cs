9047f2e4b41f4207f8c8e570b2f4e5019d6608e2
S-JOBI23-062\student <231049@st.yoshida-g.ac.jp>
2024-12-26 15:34:58 +0900

    試運転の為、キャラクターを変更。Characterスクリプトから、不要なコードやコメントを削除。Characterスクリプトに、移動アニメーションの処理を追加。

--- Client/Assets/Scripts/Character.cs
+++ Client/Assets/Scripts/Character.cs
@@ -6,8 +6,9 @@ using Shared.Interfaces.StreamingHubs;
 
 public class Character : MonoBehaviour
 {
-    //Rigidbody rb;
     FloatingJoystick floatingJoystick;//スマホ対応用スライドパッド
+    float speed = 0.6f;//歩くスピードの変数。0.6fに設定
+    Animator animator;
 
     //自分自身かどうかを判定する変数
     public bool isSelf { get; set; } = false;
@@ -16,6 +17,8 @@ public class Character : MonoBehaviour
     void Start()
     {
         Rigidbody rigidbody = GetComponent<Rigidbody>();
+        animator=GetComponent<Animator>();
+
         //floatingJoystick=GameObject.Find(/*"名前"*/).GetComponent<FloatingJoystick>();
     }
 
@@ -27,26 +30,35 @@ public class Character : MonoBehaviour
             //奥に移動
             if (Input.GetKey(KeyCode.W))
             {
-                transform.DOLocalMoveZ(1f, 0.6f).SetRelative();
+                //速度設定
+                animator.SetFloat("speed", speed);//移動した時、speed秒の速さでステータスが"speed"のものをアニメーションする
+
+                transform.DOLocalMoveZ(1f, speed).SetRelative();
+
             }
 
             //手前に移動
             if (Input.GetKey(KeyCode.S))
             {
-                transform.DOLocalMoveZ(-1f, 0.6f).SetRelative();
+                //速度設定
+                animator.SetFloat("speed", speed);//移動した時、speed秒の速さでステータスが"speed"のものをアニメーションする
+
+                transform.DOLocalMoveZ(-1f, speed).SetRelative();
             }
 
             //右に移動
             if (Input.GetKey(KeyCode.D))
             {
-                //transform.position += speed * transform.right * Time.deltaTime;
-                transform.DOLocalMoveX(1f, 0.6f).SetRelative();
+                //速度設定
+                animator.SetFloat("speed", speed);//移動した時、speed秒の速さでステータスが"speed"のものをアニメーションする
+
+                transform.DOLocalMoveX(1f, speed).SetRelative();
             }
 
             //左に移動
             if (Input.GetKey(KeyCode.A))
             {
-                transform.DOLocalMoveX(-1f, 0.6f).SetRelative();
+                transform.DOLocalMoveX(-1f, speed).SetRelative();
             }
         }
     }
