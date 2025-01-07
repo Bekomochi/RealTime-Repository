49975a852fc83ebb02dd2584a4b76c9f5bb7450f
S-JOBI23-062\student <231049@st.yoshida-g.ac.jp>
2025-01-07 18:09:35 +0900

    Particleでダメージを与える処理を追加

--- Client/Assets/Scripts/WarterGun/Particle.cs
+++ Client/Assets/Scripts/WarterGun/Particle.cs
@@ -1,14 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEngine.ParticleSystem;
+using DG.Tweening;
+using Unity.VisualScripting;
+using UnityEditorInternal;
+using UnityEngine.UI;
 
 public class Particle : MonoBehaviour
 {//水鉄砲のパーティクル用のスクリプト
 
     [SerializeField] private ParticleSystem warterParticle;
-
+    int CharacterHP = Character.HP;
+    public float invincibilityDuration = 2.0f;//無敵時間（秒）
+    private float invincibilityTimer = 0.0f;//経過時間を格納するタイマー変数(初期値0秒)
+    public Slider hpSlider;
     // Start is called before the first frame update
+
     void Start()
     {
 
@@ -24,8 +31,19 @@ public class Particle : MonoBehaviour
     /// 水の当たり判定
     /// </summary>
 
-    //public void hitWarter()
-    //{
+    public void OnParticleCollision(GameObject other)
+    {
+        if(other.gameObject.tag=="character")
+        {
+            CharacterHP -= 10;//水が当たったら、HPを10減らす
+            Debug.Log(CharacterHP);
+            hpSlider.value = CharacterHP;
 
-    //}
+            if (CharacterHP <= 0)
+            {//キャラクターのHPが0以下になったら
+                CharacterHP = 0;//キャラクターのHPを0固定にする
+                Debug.Log(CharacterHP);//キャラクターのHPをコンソールに表示して確認
+            }
+        }
+    }
 }
