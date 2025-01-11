ae0fb95d91790ce38929d7553b2ab6f95c26caac
S-JOBI23-062\student <231049@st.yoshida-g.ac.jp>
2025-01-11 16:48:39 +0900

    HPを減らしてスライダーの値を変更する処理を、ParticleからCharacterに移し替え。コメントアウトしていた部分を消去

--- Client/Assets/Scripts/Character.cs
+++ Client/Assets/Scripts/Character.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using DG.Tweening;
 using Shared.Interfaces.StreamingHubs;
+using UnityEngine.TextCore.Text;
 
 public class Character : MonoBehaviour
 {
+    [SerializeField] public Slider HPSlider;
+    [SerializeField] int CharacterHP = 100;//キャラクターの体力。適宜調整
+
     //FloatingJoystick floatingJoystick;//スマホ対応用スライドパッド
     float speed = 0.6f;//歩くスピードの変数。0.6fに設定
-    public static int HP = 100;//キャラクターの体力。適宜調整
     public bool isSelf { get; set; } = false;//自分自身かどうかを判定する変数
     Animator animator;//アニメーター取得
     public static BoxCollider boxCollider;//キャラクターのBoxColliderの変数
@@ -61,6 +65,21 @@ public class Character : MonoBehaviour
                 animator.SetFloat("speed", speed);//移動した時、speed秒の速さでステータスが"speed"のものをアニメーションする
 
                 transform.DOLocalMoveX(-1f, speed).SetRelative();
+            }
+        }
+    }
+
+    public void OnParticleCollision(GameObject other)
+    {
+        if (other.tag == "warter")
+        {
+            CharacterHP -= 10;//水が当たったら、HPを10減らす
+            HPSlider.value = CharacterHP;
+            Debug.Log(CharacterHP);
+
+            if (CharacterHP <= 0)
+            {//キャラクターのHPが0以下になったら
+                CharacterHP = 0;//キャラクターのHPを0固定にする
             }
         }
     }
